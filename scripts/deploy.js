e41d309ac22b0fef22d611ff1d11b6f6edf33b04
Brendan Giberson <brendan.giberson@blackbaud.me>
2021-10-26 16:42:05 -0400

    Created test coin and faucet

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  const Fct = await hre.ethers.getContractFactory("FCTToken");
+  const fct = await Fct.deploy("FCTToken", "TKN");
+
+  await fct.deployed();
+
+  console.log("FCT token deployed to:", fct.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import Faucet from './components/Faucet';
+import TokenSend from './components/TokenSend';
 
 function App() {
+
+  const Token = FCTToken;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+    <Row className="justify-content-md-center">
+      <Col>
+        <Faucet  tokenContract={Token}/>
+      </Col>
+      <Col>
+        <TokenSend tokenContract={Token}/>
+      </Col>
+    </Row>
+    </Container>
     </div>
   );
 }
